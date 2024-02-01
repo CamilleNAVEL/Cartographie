4b886d543167700eb45ed14baabc53afc486894f
camille <camille.navel@eleve.ensai.fr>
2024-02-01 11:46:00 +0000

    fin 1er TP

--- TP_2.R
+++ TP_2.R
@@ -1,5 +1,6 @@
 library(sf)
 library(dplyr)
+library(ggplot2)
 
 commune <- st_read("commune_francemetro_2021.shp", options = "ENCODING=WINDOWS-1252")
 
@@ -22,4 +23,53 @@ communes_bretagne <- communes_bretagne %>% mutate(surf2= units::set_units(surf2,
 cor(communes_bretagne$surf,communes_bretagne$surf2)
 # Les variables ne sont pas égales mais presque, sans doute les arrondis
 
-dept_bretagne <- communes_bretagne %>%  distinct(code) %>% select(code,surf2)
+
+dept_bretagne <- communes_bretagne %>%
+  group_by(dep) %>%
+  summarize(surf = sum(surf2))
+
+
+plot(dept_bretagne)
+
+dept_bretagne_sf <- communes_bretagne %>%
+  group_by(dep) %>%
+  summarize(geometry= st_union(geometry))
+
+plot(dept_bretagne_sf)
+
+centroid_dept_bretagne <- dept_bretagne %>% summarise(centroide = st_centroid(geometry))
+plot(centroid_dept_bretagne)
+
+ggplot() +
+  geom_sf(data = dept_bretagne_sf, fill = "lightblue", color = "black") +
+  geom_sf(data = centroid_dept_bretagne, color = "red", size = 2) +
+  theme_minimal()
+
+summary(centroid_dept_bretagne)
+centroid_dept_bretagne <- centroid_dept_bretagne %>% mutate(dept_lib =c("Côtes-d'Armor","Finistère", "Ille-et-Vilaine", "Morbihan" ))
+ggplot() +
+  geom_sf(data = dept_bretagne_sf, fill = "lightblue", color = "black") +
+  geom_sf(data = centroid_dept_bretagne, color = "red", size = 3) +
+  geom_sf_text(data = centroid_dept_bretagne, aes(label = dept_lib), color = "darkblue", size = 5, nudge_y = 10000) +
+  theme_minimal()
+
+centroid_coords <- st_coordinates(centroid_dept_bretagne)
+#centroid_coords <- bind_cols(centroid_coords,centroid_dept_bretagne$dept_lib)
+#centroid_coords <- bind_cols(centroid_coords,dept_bretagne$dep)
+
+nom_commune_bretagne <- communes_bretagne %>% select(libelle)
+intersection<-st_intersects(centroid_dept_bretagne,nom_commune_bretagne)
+summary(intersection)
+commune_centre <- NULL
+for (i in 1:4) {
+  
+commune_centre[i]<-nom_commune_bretagne[intersection[[i]],1]$libelle
+}
+  
+  
+intersection2<-st_within(centroid_dept_bretagne,communes_bretagne)
+commune_centre2 <- NULL
+for (i in 1:4) {
+  
+  commune_centre2[i]<-nom_commune_bretagne[intersection2[[i]],1]$libelle
+}
